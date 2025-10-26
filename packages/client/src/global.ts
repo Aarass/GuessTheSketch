731e287f08742bf4ef2489ef1aab59ddec04d826
Aarass <prokopovic75@gmail.com>
2025-10-27 00:21:17 +0100

    tmp

--- packages/client/src/global.ts
+++ packages/client/src/global.ts
@@ -6,7 +6,6 @@ import {
 } from "@guessthesketch/common"
 
 export const backend = "localhost:8080"
-// export const backend = "178.149.108.197:8080"
 
 export const sockets = {
   global: null as GlobalClientSocket | null,
